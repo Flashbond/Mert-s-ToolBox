c409cf99f90e7039689c75930526064d3272aeaa
Flashbond <Flashbond@users.noreply.github.com>
2026-04-02 19:19:58 +0300

    ui-draft delete

--- ui-draft/index.tsx
+++
@@ -1,8 +0,0 @@
-﻿import { ModRegistrar } from "cs2/modding";
-import { PerfectCirclePanel } from "./PerfectCirclePanel";
-
-const register: ModRegistrar = (moduleRegistry) => {
-    moduleRegistry.append("Game", PerfectCirclePanel);
-};
-
-export default register;
